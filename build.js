a63c2899437875c2eca283a1fbb83fade4ae465e
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-14 16:00:20 +0000

    react primitives

--- build.js
+++ build.js
@@ -8,10 +8,13 @@ const { format } = require('prettier');
 
 // https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs
 const isDirectory = source => lstatSync(source).isDirectory()
+const isFile = source => !lstatSync(source).isDirectory()
+
 const getDirectories = source =>
   readdirSync(source).map(name => join(source, name)).filter(isDirectory)
 
-const componentDirs = getDirectories('./node_modules/govuk-frontend/components/');
+const getFiles = source =>
+  readdirSync(source).map(name => join(source, name)).filter(isFile)
 
 const mkdir = dir => {
   if (!existsSync(dir)){
@@ -25,13 +28,9 @@ mkdir('./lib')
 mkdir('./lib/css')
 mkdir('./lib/json')
 
-
-componentDirs.map(componentDir => {
-  const componentName = parse(componentDir).name;
-  console.log(`building: ${componentName}`);
-
+function process(sassFile, componentName, outDir) {
   sassExtract.render({
-    file: `./node_modules/govuk-frontend/components/${componentName}/_${componentName}.scss`
+    file: sassFile
   })
   .then(rendered => {
     let emotion = convertCssForEmotion(rendered.css.toString());
@@ -48,15 +47,31 @@ componentDirs.map(componentDir => {
         },
     ).replace(/^css/m, "injectGlobal");
 
-    writeFileSync(`./es/components/${componentName}.js`, emotion);
-    writeFileSync(`./lib/json/${componentName}.json`, JSON.stringify(rendered.vars, null, 2));
-    writeFileSync(`./lib/css/${componentName}.css`, rendered.css.toString());
+    writeFileSync(`./es/${outDir}/${componentName}.js`, emotion);
+    // writeFileSync(`./lib/json/${componentName}.json`, JSON.stringify(rendered.vars, null, 2));
+    // writeFileSync(`./lib/css/${componentName}.css`, rendered.css.toString());
     console.log(`done: ${componentName}`);
   });
-})
+}
 
-const componentNames = componentDirs.map(componentDir => parse(componentDir).name);
+getDirectories('./node_modules/govuk-frontend/components/').map(componentDir => {
+  const componentName = parse(componentDir).name;
+  console.log(`building: ${componentName}`);
+  const sassFile = `./node_modules/govuk-frontend/components/${componentName}/_${componentName}.scss`
+  process(sassFile, componentName, 'components')
+});
 
-const indexFile = componentNames.map(name => `export * from './components/${name}';`).join('\n');
+// getFiles('./node_modules/govuk-frontend/objects/')
+//   .filter(file => parse(file).name !== '_all.scss')
+//   .map(componentFile => {
+//     const componentName = parse(componentFile).name;
+//     console.log(`building: ${componentName}`);
+//     const sassFile = `./node_modules/govuk-frontend/objects/${componentName}.scss`;
+//     process(sassFile, componentName, 'objects');
+// });
 
-writeFileSync('./es/index.js', indexFile);
+// const componentNames = componentDirs.map(componentDir => parse(componentDir).name);
+
+// const indexFile = componentNames.map(name => `export * from './components/${name}';`).join('\n');
+
+// writeFileSync('./es/index.js', indexFile);
